1d5ad6fcddfaa408dbff1fe4e9f73dbd8d47e966
Wei Zhang <X-Spirit.zhang@ttu.edu>
2017-04-28 18:13:34 -0500

    commit changes

--- hotspot_detector.py
+++ hotspot_detector.py
@@ -111,7 +111,7 @@ def main():
         xy = X[class_member_mask & core_samples_mask]
         clst.append(plt.scatter(xy[:, 0], xy[:, 1], c=col, marker=markr))
 
-    plt.legend(clst, clst_name, loc=9)
+    plt.legend(clst, clst_name, loc='upper center', bbox_to_anchor=(0.5,-0.1))
     # plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.title(plot_title)
     plt.savefig(args.output)
