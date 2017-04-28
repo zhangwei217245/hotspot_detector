ea397ee1b4246650e64eb5f37d618995fbb6818d
Wei Zhang <X-Spirit.zhang@ttu.edu>
2017-04-28 16:09:13 -0500

    commit changes

--- runExp.sh
+++ runExp.sh
@@ -2,15 +2,15 @@
 
 FILEBASE=$1
 
+head=$2
+tail=$3
+
+
 for i in $(seq 0 3);
 do
     for minpts in $(seq 100 100 500);
     do
-        for head in $(seq 0 1000000 5000000000);
-        do
-            tail=`echo "$head + 1000000"|bc`
-            echo "========= $FILEBASE${i}_${minpts}_${head}_${tail} =========="
-            ./hotspot_detector.py -H $head -T $tail -m $minpts -f $FILEBASE$i -o $FILEBASE${i}_${minpts}_${head}_${tail}.png
-        done
+        echo "========= $FILEBASE${i}_${minpts}_${head}_${tail} =========="
+        ./hotspot_detector.py -H $head -T $tail -m $minpts -f $FILEBASE$i -o $FILEBASE${i}_${minpts}_${head}_${tail}.png
     done
 done
