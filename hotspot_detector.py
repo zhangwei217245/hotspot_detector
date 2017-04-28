bd08fee729f42a2eeda26f2e506422cf24dd9abb
Wei Zhang <X-Spirit.zhang@ttu.edu>
2017-04-28 16:10:18 -0500

    commit changes

--- hotspot_detector.py
+++ hotspot_detector.py
@@ -102,11 +102,11 @@ def main():
 
         xy = X[class_member_mask & core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-                 markeredgecolor='k', mew=0.5, markersize=14)
+                 markeredgecolor='k', mew=0.5, markersize=8)
 
         xy = X[class_member_mask & ~core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-                 markeredgecolor='w', mew=0.5, markersize=6)
+                 markeredgecolor='w', mew=0.5, markersize=3)
 
     plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.savefig(args.output)
