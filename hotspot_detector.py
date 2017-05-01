989bdd22387dfefcaf5405dae8646c77d374a0a1
Wei Zhang <X-Spirit.zhang@ttu.edu>
2017-04-30 23:36:36 -0500

    commit changes

--- hotspot_detector.py
+++ hotspot_detector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys
 import os
@@ -8,7 +8,7 @@ import argparse
 import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
-
+import matplotlib.ticker as ticker
 
 def process_console_args():
     parser = argparse.ArgumentParser('hotspot_detector.py')
@@ -21,6 +21,9 @@ def process_console_args():
     args = parser.parse_args()
     return args
 
+def formatAddr(x, pos):
+    s = '{0:#0{1}X}'.format(x.astype(int),18)
+    return s
 
 def main():
     args = process_console_args()
@@ -41,72 +44,101 @@ def main():
     if len(X) == 0:
         exit(0)
 
-    db = DBSCAN(eps=args.eps, min_samples=args.minpts).fit(X)
+    db = DBSCAN(8192,50).fit(X)
+    # db = DBSCAN(eps=0.05, min_samples=3).fit(X)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
 
     # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    print('Estimated number of clusters: %d' % n_clusters_)
 
-    cluster_table = np.full((n_clusters_, len(labels)), -1, dtype = int)
-    for inst_idx in range(len(db.components_)):
-        addr = db.components_[inst_idx][1]
-        label_idx = db.core_sample_indices_[inst_idx]
-        inst_label = labels[label_idx]
-        cluster_table[inst_label, label_idx] = addr
-
-    clst_name=[]
-    for cluster_idx in range(n_clusters_):
-        min = 0xffffffff
-        max = 0x00000000
-        num_inst = 0
-        for addr_ in cluster_table[cluster_idx]:
-            if addr_ >= 0x00000000:
-                num_inst = num_inst + 1
-                if min > addr_:
-                    min = addr_
-                if max < addr_:
-                    max = addr_
-        clst_name.append("C{:02}({}~{}, inst={})".format(cluster_idx,"{0:#0{1}X}".format(min,18),"{0:#0{1}X}".format(max,18), num_inst))
-        print("For cluster ", "{:02}".format(cluster_idx), ", the range = (", "{0:#0{1}X}".format(min,18), " ~ ", "{0:#0{1}X}".format(max,18), "), number of instances = ", num_inst)
-
-
-    num_outlier = 0
-    for label_ in labels:
-        if label_ == -1:
-            num_outlier = num_outlier + 1
-
-    plot_title = "{}, num_clusters={}, \nnum_outliers={}/{}, num_core_samples={}/{}"\
-        .format(args.output, n_clusters_, num_outlier, len(labels), len(db.core_sample_indices_), len(labels))
-    print("The total number of outliers = ", num_outlier, ", the number of all core samples = ", len(db.core_sample_indices_), ", the number of all instances =", len(labels))
-
-    # print("Silhouette Coefficient: %0.3f"
-    #       % metrics.silhouette_score(X, labels))
 
     # Black removed and is used for noise instead.
     unique_labels = set(labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
-    clst = []
-    markr = "o"
     for k, col in zip(unique_labels, colors):
-        class_member_mask = (labels == k)
         if k == -1:
             # Black used for noise.
             col = 'k'
-            markr = 'x'
-            xy = X[class_member_mask & ~core_samples_mask]
-            plt.scatter(xy[:, 0], xy[:, 1], c=col, marker=markr)
-        else:
-            xy = X[class_member_mask & core_samples_mask]
-            clst.append(plt.scatter(xy[:, 0], xy[:, 1], c=col, marker=markr))
 
-    lgd = plt.legend(clst, clst_name, loc='upper center', bbox_to_anchor=(0.5,-0.1))
-    # plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.title(plot_title)
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'x', markerfacecolor=col,
+                 markeredgecolor='k', markersize=6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    axes = plt.gca()
+    axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(formatAddr))
+    print('Estimated number of clusters: %d' % n_clusters_)
+    # plt.yticks(rotation=30)
     plt.tight_layout()
-    plt.savefig(args.output, bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.show()
+
+
+
+    # cluster_table = np.full((n_clusters_, len(labels)), -1, dtype = int)
+    # for inst_idx in range(len(db.components_)):
+    #     addr = db.components_[inst_idx][1]
+    #     label_idx = db.core_sample_indices_[inst_idx]
+    #     inst_label = labels[label_idx]
+    #     cluster_table[inst_label, label_idx] = addr
+    #
+    # clst_name=[]
+    # for cluster_idx in range(n_clusters_):
+    #     min = 0xffffffff
+    #     max = 0x00000000
+    #     num_inst = 0
+    #     for addr_ in cluster_table[cluster_idx]:
+    #         if addr_ >= 0x00000000:
+    #             num_inst = num_inst + 1
+    #             if min > addr_:
+    #                 min = addr_
+    #             if max < addr_:
+    #                 max = addr_
+    #     clst_name.append("C{:02}({}~{}, inst={})".format(cluster_idx,"{0:#0{1}X}".format(min,18),"{0:#0{1}X}".format(max,18), num_inst))
+    #     print("For cluster ", "{:02}".format(cluster_idx), ", the range = (", "{0:#0{1}X}".format(min,18), " ~ ", "{0:#0{1}X}".format(max,18), "), number of instances = ", num_inst)
+    #
+    #
+    # num_outlier = 0
+    # for label_ in labels:
+    #     if label_ == -1:
+    #         num_outlier = num_outlier + 1
+    #
+    # plot_title = "{}, num_clusters={}, \nnum_outliers={}/{}, num_core_samples={}/{}"\
+    #     .format(args.output, n_clusters_, num_outlier, len(labels), len(db.core_sample_indices_), len(labels))
+    # print("The total number of outliers = ", num_outlier, ", the number of all core samples = ", len(db.core_sample_indices_), ", the number of all instances =", len(labels))
+    #
+    # # print("Silhouette Coefficient: %0.3f"
+    # #       % metrics.silhouette_score(X, labels))
+    #
+    # # Black removed and is used for noise instead.
+    # unique_labels = set(labels)
+    # colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    # clst = []
+    # markr = "o"
+    # for k, col in zip(unique_labels, colors):
+    #     class_member_mask = (labels == k)
+    #     if k == -1:
+    #         # Black used for noise.
+    #         col = 'k'
+    #         markr = 'x'
+    #         xy = X[class_member_mask & ~core_samples_mask]
+    #         plt.scatter(xy[:, 0], xy[:, 1], c=col, marker=markr)
+    #     else:
+    #         xy = X[class_member_mask & core_samples_mask]
+    #         clst.append(plt.scatter(xy[:, 0], xy[:, 1], c=col, marker=markr))
+    #
+    # lgd = plt.legend(clst, clst_name, loc='upper center', bbox_to_anchor=(0.5,-0.1))
+    # # plt.title('Estimated number of clusters: %d' % n_clusters_)
+    # plt.title(plot_title)
+    # plt.tight_layout()
+    # plt.savefig(args.output, bbox_extra_artists=(lgd,), bbox_inches='tight')
 
     return
 
